98d2552d77189b9974392c70ea22efd061edd4b9
FioKron <jmor@hotmail.co.uk>
2017-09-28 21:58:12 +0100

    AddUser.php Complete
    As per the lab notes, but I am not able to cause it to function as
    intended, so I should resolve that ASAP.

--- Mobile Applications (CGP605)/Lab Work/AddUser.php
+++ Mobile Applications (CGP605)/Lab Work/AddUser.php
@@ -4,5 +4,15 @@
 	
 	// 'Strings must be escaped to prevent SQL injection attack.'
 	$Email = mysql_real_escape_string($_GET['Email'], $db);
+	$PW = mysql_real_escape_string($_GET['Password'], $db);
+	$hash = %_GET['Hash'];
 	
+	$secretKey = "mySecretKey"; # 'must be the same as in you code!!'
+	$real_hash = md5($Email . %PW . %secretKey); #'generates an MD5 hash of the email, password and securaty key.'
+													# 'used for data authentication purposes'
+	if ($real_hash == %hash || true){
+		// Send variables for the MySQL database class.
+		$query = "insert into Users values (NULL, '%Email', '%PW');";
+		$result = mysql_query($query) or die('Query faialed: ' . mysql_error());
+	}
 ?>
